967c09fdd7242a0a8a1123c9b46ad35a999ad8bc
hongchenjie <chenjie.hong@newland.com.cn>
2025-01-02 16:58:39 +0800

    feat: 添加默认参数的值

--- poolify-core/src/main/java/cn/poolify/core/properties/entity/DtpExecutorProps.java
+++ poolify-core/src/main/java/cn/poolify/core/properties/entity/DtpExecutorProps.java
@@ -9,13 +9,16 @@ import java.util.concurrent.TimeUnit;
  * @Author: HCJ
  * @DateTime: 2024/12/25
  * @Description: 动态线程池属性
- * TODO: 配置默认值
  **/
 @Data
 public class DtpExecutorProps extends TpExecutorProps{
+    /** 线程池名称 -- bean name */
     private String threadPoolName;
-    private String executorType;
-    private long queueTimeout;
-    private long runTimeout;
+    /** 线程池类型 -- 默认default TODO: 暂时只有default类型 */
+    private String executorType = "default";
+    /** 等待超时时间 */
+    private long queueTimeout = 100;
+    /** 运行超时时间 */
+    private long runTimeout = 100;
 
 }
--- poolify-core/src/main/java/cn/poolify/core/properties/entity/TpExecutorProps.java
+++ poolify-core/src/main/java/cn/poolify/core/properties/entity/TpExecutorProps.java
@@ -7,15 +7,20 @@ import java.util.concurrent.TimeUnit;
 /**
  * @Author: HCJ
  * @DateTime: 2024/12/30
- * @Description:
+ * @Description: 线程池参数
  **/
 @Data
 public class TpExecutorProps {
-    private long keepAliveTime;
-    private boolean allowCoreThreadTimeOut;
+    /** 核心线程数 */
     private int corePoolSize;
+    /** 最大线程数 */
     private int maximumPoolSize;
-    private TimeUnit unit;
+    /** 非核心线程存活时间 */
+    private long keepAliveTime;
+    /** 存活时间 */
+    private TimeUnit unit = TimeUnit.MICROSECONDS;
+    /** 核心线程是否过期 */
+    private boolean allowCoreThreadTimeOut = false;
 
     public boolean coreParamIsInValid() {
         return this.getCorePoolSize() < 0
