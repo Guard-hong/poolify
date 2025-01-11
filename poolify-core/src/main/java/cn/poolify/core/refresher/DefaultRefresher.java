fed8e0bf0af222eb94b1aa1f1f1b369a5f102e5e
hongchenjie <chenjie.hong@newland.com.cn>
2025-01-11 16:20:00 +0800

    fit: 修改刷新机制类结构和职责

--- poolify-core/src/main/java/cn/poolify/core/refresher/DefaultRefresher.java
+++ poolify-core/src/main/java/cn/poolify/core/refresher/DefaultRefresher.java
@@ -1,29 +1,36 @@
 package cn.poolify.core.refresher;
 
 import cn.poolify.core.properties.DtpProperties;
+import org.springframework.cloud.context.environment.EnvironmentChangeEvent;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.event.SmartApplicationListener;
 
 /**
  * @Author: HCJ
  * @DateTime: 2025/1/10
  * @Description:
  **/
-public class DefaultRefresher extends AbstractRefresher {
+public class DefaultRefresher extends AbstractRefresher implements SmartApplicationListener {
     private final static String REFRESHER = "default";
 
     public DefaultRefresher(DtpProperties dtpProperties) {
         super(dtpProperties);
     }
 
-    /**
-     * default not notify
-     */
-    @Override
-    protected void doNotify() {
-
-    }
-
     @Override
     public String mark() {
         return REFRESHER;
+    }
+
+    @Override
+    public boolean supportsEventType(Class<? extends ApplicationEvent> eventType) {
+        return EnvironmentChangeEvent.class.isAssignableFrom(eventType);
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+        if (needRefresh(((EnvironmentChangeEvent) event).getKeys())) {
+            refresh();
+        }
     }
 }
