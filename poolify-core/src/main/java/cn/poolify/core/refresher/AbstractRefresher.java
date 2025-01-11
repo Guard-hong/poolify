02b7a17fa7ea2633e213f9aa388e259e6023c6f3
hongchenjie <chenjie.hong@newland.com.cn>
2025-01-11 16:05:30 +0800

    feat: 添加配置

--- poolify-core/src/main/java/cn/poolify/core/refresher/AbstractRefresher.java
+++ poolify-core/src/main/java/cn/poolify/core/refresher/AbstractRefresher.java
@@ -3,12 +3,12 @@ package cn.poolify.core.refresher;
 import cn.poolify.core.executor.DtpRegistry;
 import cn.poolify.core.properties.DtpProperties;
 import cn.poolify.core.utils.BinderUtils;
+import cn.poolify.core.utils.CollectionUtils;
 import org.springframework.cloud.context.environment.EnvironmentChangeEvent;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.event.SmartApplicationListener;
 import org.springframework.core.env.Environment;
-import org.springframework.util.CollectionUtils;
 
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -69,7 +69,7 @@ public abstract class AbstractRefresher implements IRefresher, SmartApplicationL
         changedKeys = changedKeys.stream()
                 .filter(str -> str.startsWith(DTP_EXECUTOR_PROP))
                 .collect(Collectors.toSet());
-        return !CollectionUtils.isEmpty(changedKeys);
+        return CollectionUtils.isNotEmpty(changedKeys);
     }
 
 }
